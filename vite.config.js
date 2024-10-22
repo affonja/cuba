da4031e1e70cf7f84cd7db69d4a5e78ca3bec9da
affonja <56819481+affonja@users.noreply.github.com>
2024-10-23 01:09:22 +0400

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                // 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
